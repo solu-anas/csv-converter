73b97b9c515b7e9d1ea7ee99bd259fbb1c821baf
solu-anas <a.elbtioui@solusaas.com>
2024-01-02 15:50:36 +0100

    test

--- playground.js
+++ playground.js
@@ -1,40 +1,43 @@
-const map = new Map(
-    [
-        ["nom", "name"],
-        ["adresse", "address"],
-        ["âge", "age"],
-        ["code postal", "zipCode"],
-        ["e-mail", "email"]
-    ]
-)
-
-map.get(key)
-map.keys(key)
-let counter1 = 0;
-for (let key of map.keys()) {
-    counter1++;
-    console.log(counter1, key);
+async function streamFile2(filePath, promiseThunk, bufferSize = 100) {
+    return new Promise((resolve, reject) => {
+        //create the NodeJS read stream
+        const stream = fs.createReadStream(filePath, { encoding: 'utf8' });
+        //how many lines should we process at a time?
+        let buffer = [];
+        stream
+            //ensure parsing line by line
+            .pipe(split2())
+            /*ensure that the next chunk will be processed by the
+            stream only when we want to*/
+            .pipe(
+                through2((chunk, enc, callback) => {
+                    //put the chunk along with the other ones
+                    buffer.push(chunk.toString());
+                    if (buffer.length < bufferSize) {
+                        callback(); //next step, no process
+                    } else {
+                        //call the method that creates a promise, and at the end
+                        //just empty the buffer, and process the next chunk
+                        promiseThunk(buffer).finally(() => {
+                            buffer = [];
+                            callback();
+                        });
+                    }
+                }),
+            )
+            .on('error', error => {
+                reject(error);
+            })
+            .on('finish', () => {
+                //any remaining data still needs to be sent
+                //resolve the outer promise only when the final batch has completed processing
+                if (buffer.length > 0) {
+                    promiseThunk(buffer).finally(() => {
+                        resolve(true);
+                    });
+                } else {
+                    resolve(true);
+                }
+            });
+    });
 }
-
-let counter2 = 0;
-for (let value of map.values()) {
-    counter2++;
-    console.log(counter2, value);
-}
-
-let counter3 = 0;
-for (let [key, value] of map.entries()) {
-    ++counter3;
-    console.log(counter3, [key, value]);
-}
-
-const fs = require('fs');
-
-// csv parser
-const csv = require('csv-parser');
-const spectrum = require('csv-spectrum');
-
-spectrum((err, data) => {
-    if (err) return console.error(err.message);
-    console
-});
--- routes/inserts.js
+++ routes/inserts.js
@@ -2,16 +2,17 @@ const { Table } = require('../models/table');
 const { Person } = require('../models/person');
 const { Operation } = require('../models/operation');
 const { join } = require('path');
-const readline = require('readline');
 const express = require('express');
 const verifyToken = require('../middleware/verifyToken');
-const fields = require('../fields.json');
 const router = express.Router();
-
 const fs = require('fs');
+const { finished } = require('stream/promises');
+const { parse } = require('csv-parse');
 const csv = require('csv-parser');
+const _ = require('lodash');
+const through2 = require('through2');
 
-router.get('/check-progress', async (req, res) => {
+/*router.get('/check-progress', async (req, res) => {
     const table = await Table.findOne({ uuid: req.body.tableUUID });
 
     if (!table) {
@@ -27,70 +28,95 @@ router.get('/check-progress', async (req, res) => {
             console.error("Operation Query Error:", err.message)
             res.status(400).send("Operation Query Error");
         })
-})
+})*/
 
 router.post('/start', verifyToken, async (req, res) => {
 
-    Table.findOne({ uuid: req.body.tableUUID }).then(async (table) => {
-        const insert = new Operation({
-            type: "insert",
-            user: req.user._id,
-            table: table._id,
-            details: {
-                progress: 0,
-                isProgressEnd: false
-            }
-        });
-        insert.save()
-            .then((savedInsert) => {
-                const mapping = new Map(req.body.mapping);
-                let progressCounter = 0;
-                res.send('started')
-                const fileLocation = join(__dirname, `../tables/${table.uuid}.csv`);
-                const readInterface = readline.createInterface({
-                    input: fs.createReadStream(fileLocation),
-                    output: process.stdout,
-                    console: false
-                });
-                readInterface
-                    .on('line', (line) => {
-                        line.pipe(csv())
-                            .on('data', (row) => {
-                                // Computation
-                                let document;
-                                try {
-                                    document = fields.reduce((doc, field) => {
-                                        doc[field] = row[mapping.get(field)]
-                                        return doc;
-                                    }, {});
-                                    console.log(document);
-                                }
-                                catch (err) {
-                                    console.error("Insertion Error:", err.message)
-                                }
-                                (new Person(document)).save()
-                                    .then(() => {
-                                        Operation.findByIdAndUpdate(savedInsert._id, {
-                                            details: { progress: ++progressCounter }
-                                        }).catch((err) => { console.error("Insertion Error:", err.message) })
+    Table
+        .findOne({ uuid: req.body.tableUUID })
+        .then(async (table) => {
+            const insert = new Operation({
+                type: "insert",
+                user: req.user._id,
+                table: table._id,
+                details: {
+                    progress: 0,
+                    isProgressEnd: false
+                }
+            });
+            insert
+                .save()
+                .then((savedInsert) => {
+                    const mapping = new Map(req.body.mapping);
+                    let progressCounter = 0;
+                    const fields = require('../fields.json');
+                    const fileLocation = join(__dirname, `../tables/${table.uuid}.csv`);
+                    // const parser = fs.createReadStream(fileLocation).pipe(parse());
+
+                    const results = [];
+                    
+                    const parser = fs.createReadStream(fileLocation).pipe(csv());
+                    
+                    let buffer = [];
+                    const bufferSize = 100;
+                    
+                    parser
+                        .pipe(through2.obj((chunk, enc, cb) => {
+                            const document = _.mapKeys(chunk, (value, key) => mapping.get(key));
+                            buffer.push(document);
+                            if (buffer.length < bufferSize) cb();
+                            else {
+                                // empty the buffer
+                                buffer = [];
+                                cb();
+                            }
+                            
+                            console.log(document);
+                                (new Person(document))
+                                .save()
+                                .then(() => {
+                                    Operation.findByIdAndUpdate(savedInsert._id, {
+                                        details: { progress: ++progressCounter }
+                                    }).catch((err) => {
+                                        console.error("Insertion Error:", err.message)
+                                        return res.status(500).json({ Error: err.message })
                                     })
-                                    .catch((err) => console.error("Insertion Error:", err.message))
-                            })
+                                })
+                                .catch((err) => {
+                                    console.error("Insertion Error:", err.message)
+                                    return res.status(500).json({ Error: err.message })
+                                })
+                            cb();
+                        }))
 
-                    })
-                    .on('close', () => {
-                        Operation.findByIdAndUpdate(savedInsert._id, {
-                            details: { isProgressEnd: true }
-                        }).catch((err) => { console.error("Insertion Error:", err.message) })
+                        .on('error', (err) => console.error('Error:', err.message))
 
-                    })
-                    .on('error', (err) => {
-                        console.error(`Error: ${err}`);
-                    });
-            })
-            .catch((err) => { console.log("Insertion Error:", err.message) });
+                        .on('end', () => {
+                            console.log(results);
+                            return res.json({ newlyAdded: results.length })
+                            //     Operation.findByIdAndUpdate(savedInsert._id, {
+                            //         details: { isProgressEnd: true }
+                            //     }).catch((err) => {
+                            //         console.error("Insertion Error:", err.message)
+                            //         return res.status(500).json({ Error: err.message })
+                        })
 
-    })
+                    // })
+                    // .on('error', (err) => {
+                    //     console.error(`Error: ${err}`);
+                    //     return res.status(500).json({ Error: err.message })
+                    // });
+                })
+                .catch((err) => {
+                    console.log("Insertion Error:", err.message)
+                    return res.status(500).json({ Error: err.message })
+                });
+
+        })
 })
+
+function promiseThunk(lineBatch) {
+    return new Promise();
+}
 
 module.exports = router
